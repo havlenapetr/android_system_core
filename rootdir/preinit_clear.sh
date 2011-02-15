2840ccb887eeafb76ad88bb11cd94f56cf560854
havlenapetr <havlenapetr@gmail.com>
2011-02-15 15:28:50 +0100

    add forgotten files for preinit mode
    Change-Id: I18700b4c335fd79f37397e1a647045b107a5ab7d

---
+++ rootdir/preinit_clear.sh
@@ -0,0 +1,11 @@
+umount /data/local/system
+umount /data/local/data
+umount /data/local/dbdata
+umount /data/local/cache
+umount /data/local/sdroot
+
+rmdir /data/local/system
+rmdir /data/local/data
+rmdir /data/local/dbdata
+rmdir /data/local/cache
+rmdir /data/local/sdroot
