ff67144c61b06a3b7dba590ac0ab60a6afbb1da9
Petr Havlena <havlenapetr@gmail.com>
2011-10-11 19:57:22 +0200

    toolbox - add daemon mode + output to file into dmesg

--- toolbox/dmesg.c
+++ toolbox/dmesg.c
@@ -2,42 +2,86 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/klog.h>
 #include <string.h>
 
 #define KLOG_BUF_SHIFT	17	/* CONFIG_LOG_BUF_SHIFT from our kernel */
 #define KLOG_BUF_LEN	(1 << KLOG_BUF_SHIFT)
 
+int fd;
+
+static void dmesg_close() {
+    if (fd != STDOUT_FILENO && fd != 0) {
+        close(fd);
+        fd = 0;
+    }
+}
+
+static void dmesg_os_handler(int signum, siginfo_t *info, void *ptr) {
+    //printf("Received signal %d\n", signum);
+    //printf("Signal originates from process %lu\n",
+    //       (unsigned long)info->si_pid);
+    dmesg_close();
+}
+
 int dmesg_main(int argc, char **argv)
 {
     char buffer[KLOG_BUF_LEN + 1];
+    struct sigaction act;
     char *p = buffer;
     ssize_t ret;
     int n, op;
 
-    if((argc == 2) && (!strcmp(argv[1],"-c"))) {
-        op = KLOG_READ_CLEAR;
-    } else {
-        op = KLOG_READ_ALL;
-    }
+    do {
+        if (argc > 1 && !strcmp(*argv, "-c")) {
+            op = KLOG_READ_CLEAR;
+        } else if (argc > 1 && !strcmp(*argv, "-d")) {
+            op = KLOG_READ;
+        } else if (argc > 1 && !strcmp(*argv, "-f")) {
+            *argv++;
+            fd = open(*argv, O_WRONLY | O_CREAT);
+        } else {
+            op = KLOG_READ_ALL;
+            fd = 0;
+        }
+        *argv++;
+    } while (*argv);
 
-    n = klogctl(op, buffer, KLOG_BUF_LEN);
-    if (n < 0) {
-        perror("klogctl");
-        return EXIT_FAILURE;
-    }
-    buffer[n] = '\0';
+    // init os signal handler
+    memset(&act, 0, sizeof(act));
+    act.sa_sigaction = dmesg_os_handler;
+    act.sa_flags = SA_SIGINFO;
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGKILL, &act, NULL);
 
-    while((ret = write(STDOUT_FILENO, p, n))) {
-        if (ret == -1) {
-	    if (errno == EINTR)
-                continue;
-	    perror("write");
-	    return EXIT_FAILURE;
-	}
-	p += ret;
-	n -= ret;
-    }
+    do {
+        memset(&buffer, 0, KLOG_BUF_LEN + 1);
+        p = buffer;
+        n = klogctl(op, buffer, KLOG_BUF_LEN);
+        if (n < 0) {
+            perror("klogctl");
+            return EXIT_FAILURE;
+        }
+        buffer[n] = '\0';
 
+        if (fd == 0) {
+            fd = STDOUT_FILENO;
+        }
+
+        while((ret = write(fd, p, n))) {
+            if (ret == -1) {
+	            if (errno == EINTR)
+                    continue;
+	            perror("write");
+	            return EXIT_FAILURE;
+            }
+            p += ret;
+            n -= ret;
+        }
+    } while (op == KLOG_READ);
+
+    dmesg_close();
     return 0;
 }
